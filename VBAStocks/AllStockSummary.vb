6390aac67172c3366509e2dcb4356ad36dd8fde7
Miguel S. Flores <msflores10307@gmail.com>
2020-05-30 15:05:38 -0400

    adding comments to code and updating readme.

--- VBAStocks/AllStockSummary.vb
+++ VBAStocks/AllStockSummary.vb
@@ -1,12 +1,12 @@
 Sub AllStockSummary()
 
 Dim Current As Worksheet
-For Each Current In Worksheets
+For Each Current In Worksheets ' CHALLENGE 2: This loop cycles through and creates the results charts for each sheet
 
 ' activate current sheet
 Current.Activate
 
-'Create Stocks Summary
+' Create Stocks Summary
 Dim i As Long
 Dim maxrow As Long
 Dim ResultIndex As Integer
@@ -39,7 +39,7 @@ Range("P3").Style = "Percent"
 ResultIndex = 2
 tCounter = 0
 
-For i = 2 To maxrow
+For i = 2 To maxrow ' This loop cycles through and adds a row to the results for each ticker symbole
 
     If Range("A" & (i)) <> Range("A" & (i + 1)) Then
         'Ticker
@@ -51,7 +51,7 @@ For i = 2 To maxrow
         Range("J" & ResultIndex).Value = closePrice - openPrice
       
         
-        ' Percent Change - accounts for possibility of Zeros in denominator of calculation or
+        ' Percent Change - accounts for possibility of Zeros in denominator of calculation or 0% change
         If openPrice = 0 Or (closePrice - openPrice) = 0 Then
             Range("K" & ResultIndex).Value = 0
         Else
@@ -81,6 +81,7 @@ For i = 2 To maxrow
     End If
 Next i
 
+' CHALLENGE 2: The Code below creates and displays max%Change, min%Change, and maxVolume for each sheet.
 Dim minP As Double
 Dim maxP As Double
 Dim maxV As Double
@@ -89,16 +90,19 @@ Dim maxPInd As Boolean
 Dim minPInd As Boolean
 Dim maxVind As Boolean
 
+' These variables indicate if the desired values have been found in the results.
 maxPInd = False
 minPInd = False
 maxVind = False
 
+' Calculates min and max percent change, and max volume based on results produced.
 maxP = WorksheetFunction.Max(Range("K2:K" & ResultIndex))
 minP = WorksheetFunction.Min(Range("K2:K" & ResultIndex))
 maxV = WorksheetFunction.Max(Range("L2:L" & ResultIndex))
 
+' This loop iterates through results 
 For j = 2 To ResultIndex
-' checks for Maximum Percent Change
+' Searches for Maximum Percent Change
     If maxP = Range("K" & j) And maxPInd = False Then
         maxPInd = True
         Range("O2").Value = Range("I" & j)
@@ -106,7 +110,7 @@ For j = 2 To ResultIndex
     Else
         maxPInd = False
     End If
-' Checks for Minimum Percent Change
+' Searches for Minimum Percent Change
     If minP = Range("K" & j) And minPInd = False Then
         minPInd = True
         Range("O3").Value = Range("I" & j)
@@ -114,7 +118,7 @@ For j = 2 To ResultIndex
     Else
         minPInd = False
     End If
-' Checks for Maximum Volume
+' Searches for Maximum Volume
     If maxV = Range("L" & j) And maxVind = False Then
         maxPInd = True
         Range("O4").Value = Range("I" & j)
@@ -125,26 +129,6 @@ For j = 2 To ResultIndex
 Next j
 
 
-Next
-
-End Sub
-
-
-
-Sub testMe()
-
-
-Dim Current As Worksheet
-Dim wsCount As Integer
-Dim i As Integer
-
-wsCount = ActiveWorkbook.Worksheets.Count
-
-For Each Current In Worksheets
-
-Current.Activate
-Range("O4") = "Hello World"
-
-Next
+Next ' finalizes the master loop that cycles through all sheets
 
 End Sub
